889f11fa31e6d78303d84d1172998c2ebcc79e09
Clouds Flowing <clouds.flowing@gmail.com>
2023-05-24 00:20:27 +0800

    fetch: new model

--- fetch/src/main.rs
+++ fetch/src/main.rs
@@ -1,27 +1,29 @@
-use primitive_types::U256;
+use primitive_types::{U256, H256, H160};
 
 mod conn;
 mod model;
+mod rpc;
 
 // TODO: switch to read with confy
 static TABLE_EVENTS: &str = "tx_event_test";
 static TABLE_TXMSGS: &str = "tx_message_test";
 
 async fn run() -> anyhow::Result<()> {
-  let log = model::TxReceiptLog {
-    id: 0,
-    tx_idx: 0,
-    tx_message_hash: U256::zero(),
-    address: U256::zero(),
+  let log = model::Event {
+    block_number: 0,
+    idx_in_block: 0,
+    transaction: H256::zero(),
+    account: H160::zero(),
     data_len: 0,
-    data_prefix: U256::zero(),
+    data_prefix_u256: U256::zero(),
+    data_prefix_128bytes: vec![],
     topic_num: 0,
-    topic0: U256::zero(),
-    topic1: U256::zero(),
-    topic2: U256::zero(),
-    topic3: U256::zero(),
-    topic4: U256::zero(),
-  };
+    topic0: H256::zero(),
+    topic1: H256::zero(),
+    topic2: H256::zero(),
+    topic3: H256::zero(),
+    topic4: H256::zero(),
+};
   let config = toml::from_str(std::fs::read_to_string("config.toml")?.as_str())?;
   println!("config: {config:?}");
   let client = conn::clkhs_init_client(&config).await?;
--- fetch/src/model.rs
+++ fetch/src/model.rs
@@ -1,79 +1,186 @@
-use primitive_types::U256;
-use serde::{Serialize, Deserialize};
+use ethers_core::types::H32;
+use primitive_types::{U256, H160, H256};
+use serde::{Serialize, Deserialize, Deserializer, Serializer};
 use clickhouse::Row;
+use serde_with::{DeserializeAs, SerializeAs};
 
-mod u256 {
-  use primitive_types::U256;
-  use serde::{
-    de::{Deserialize, Deserializer},
-    ser::{Serialize, Serializer},
+macro_rules! serde_bytes {
+  ($proxy:ident, $T:ty, $as:ty) => {
+    serde_bytes!($proxy, $T, $as, i, <$T>::from(i), <$T>::into(i.clone()));
   };
+  ($proxy:ident, $T:ty, $as:ty, $t:ident, $from:expr, $to:expr) => {
+    struct $proxy;
 
-  pub fn serialize<S: Serializer>(u: &U256, serializer: S) -> Result<S::Ok, S::Error> {
-    let mut buf: [u8; 32] = [0; 32];
-    u.to_little_endian(&mut buf);
-    buf.serialize(serializer)
-  }
-
-  pub fn deserialize<'de, D>(deserializer: D) -> Result<U256, D::Error>
-  where
-    D: Deserializer<'de>,
-  {
-    let u: [u8; 32] = Deserialize::deserialize(deserializer)?;
-    Ok(U256::from_little_endian(&u))
-  }
+    impl<'de> DeserializeAs<'de, $T> for $proxy {
+      fn deserialize_as<D>(deserializer: D) -> Result<$T, D::Error>
+      where
+        D: Deserializer<'de>,
+      {
+        let $t = <$as>::deserialize(deserializer)?;
+        Ok($from)
+      }
+    }
+    impl SerializeAs<$T> for $proxy {
+      fn serialize_as<S>($t: &$T, serializer: S) -> Result<S::Ok, S::Error>
+      where
+        S: Serializer,
+      {
+        let u: $as = $to;
+        u.serialize(serializer)
+      }
+    }
+  };
 }
 
-#[derive(Debug, Row, Serialize, Deserialize)]
-pub struct TxReceiptLog {
-  pub id: u64,
-  pub tx_idx: u64,
-  #[serde(with = "u256")]
-  pub tx_message_hash: U256,
-  #[serde(with = "u256")]
-  pub address: U256,
-  pub data_len: u64,
-  #[serde(with = "u256")]
-  pub data_prefix: U256,
-  pub topic_num: u8,
-  #[serde(with = "u256")]
-  pub topic0: U256,
-  #[serde(with = "u256")]
-  pub topic1: U256,
-  #[serde(with = "u256")]
-  pub topic2: U256,
-  #[serde(with = "u256")]
-  pub topic3: U256,
-  #[serde(with = "u256")]
-  pub topic4: U256,
-}
+serde_bytes!(h160, H160, [u8; 20]);
+serde_bytes!(h256, H256, [u8; 32]);
+serde_bytes!(u256, U256, [u8; 32], i, U256::from_little_endian(&i), { let mut buf = [0u8; 32]; i.to_little_endian(&mut buf); buf });
+// serde_bytes!(impl h160 for primitive_types::H160);
+// serde_bytes!(impl h32 for ethers_core::types::H32);
 
-#[derive(Debug, Row, Serialize, Deserialize)]
-pub struct TxMessage{
-  pub idx: u64,
+// pub type H160Proxy = AsBytes<H160, [u8; 20]>;
+// pub type H256Proxy = AsBytes<H256, [u8; 32]>;
+
+pub type TrieHash = H256; // H256
+pub type TxHash = H256; // H256
+pub type Topic = H256; // H256
+pub type Nonce = u64;
+pub type BlockHash = H256; // H256
+pub type Address = H160; // H160
+
+pub type BigInt = U256;
+pub type GasPrice = U256;
+
+/// A log produced by a transaction.
+#[serde_with::serde_as]
+#[derive(Debug, Clone, Row, PartialEq, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Event {
   pub block_number: u64,
   pub idx_in_block: u64,
-  #[serde(with = "u256")]
-  pub hash: U256,
-  #[serde(with = "u256")]
-  pub from: U256, // txsender
-  #[serde(with = "u256")]
-  pub to: U256,
-  pub nonce: u64,
+  /// Transaction Hash
+  #[serde_as(as = "h256")]
+  pub transaction: TxHash,
+  #[serde_as(as = "h160")]
+  pub account: Address,
+  pub data_len: u64,
+  #[serde_as(as = "u256")]
+  pub data_prefix_u256: U256,
+  pub data_prefix_128bytes: Vec<u8>,
+  pub topic_num: u8,
+  #[serde_as(as = "h256")]
+  pub topic0: Topic,
+  #[serde_as(as = "h256")]
+  pub topic1: Topic,
+  #[serde_as(as = "h256")]
+  pub topic2: Topic,
+  #[serde_as(as = "h256")]
+  pub topic3: Topic,
+  #[serde_as(as = "h256")]
+  pub topic4: Topic,
+}
+
+/// Description of a Transaction, pending or in the chain.
+#[serde_with::serde_as]
+#[derive(Debug, Clone, Row, PartialEq, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct Transaction {
+  pub block_number: u64,
+  pub idx_in_block: u64,
+  #[serde_as(as = "h256")]
+  pub hash: TxHash,
+  /// Sender
+  #[serde_as(as = "h160")]
+  pub from: Address, // txsender
+  /// Recipient (or contract address when creation)
+  #[serde_as(as = "h160")]
+  pub to: Address,
+  #[serde_as(as = "u256")]
+  pub nonce: U256,
   pub gas_limit: u64,
   pub gas_used: u64, //receipt
-  #[serde(with = "u256")]
-  pub gas_priority_fee: U256,
-  #[serde(with = "u256")]
-  pub gas_fee: U256,
-  #[serde(with = "u256")]
-  pub transfer: U256,
+  /// max_priority_fee_per_gas
+  #[serde_as(as = "u256")]
+  pub gas_tip: GasPrice,
+  /// gas_price or max_fee_per_gas
+  #[serde_as(as = "u256")]
+  pub gas_fee: GasPrice,
+  // effective_tip is the actual amount of reward going to miner after considering the max fee cap.
+  pub effective_tip: Option<GasPrice>,
+  /// Effective gas price
+  pub effective_fee: Option<GasPrice>,
+  /// Transfered value
+  #[serde_as(as = "u256")]
+  pub value: BigInt,
   pub input_len: u64,
-  pub input_first_4bytes: u32,
-  #[serde(with = "u256")]
-  pub input_last_32bytes: U256,
+  pub input_first_4bytes: H32,
+  pub input_last_32bytes: [u8; 32],
   pub chain_id: Option<u64>,
   pub is_create: bool,
   pub success: bool, // receipt
   pub logs_count: u64, // receipt
+  /// ECDSA recovery id
+  pub v: u8,
+  /// ECDSA signature r, 32 bytes
+  pub r: BigInt,
+  /// ECDSA signature s, 32 bytes
+  pub s: BigInt,
+  /// Transaction type, Some(1) for AccessList transaction, None for Legacy
+  pub has_access_list: bool,
+}
+
+/// The block header type returned from GraphQL calls.
+#[serde_with::serde_as]
+#[derive(Debug, Clone, Row, PartialEq, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct Block {
+  /// Block number. None if pending.
+  pub number: u64,
+  #[serde_as(as = "h256")]
+  /// Hash of the block
+  pub hash: BlockHash,
+  #[serde_as(as = "Option<h256>")]
+  /// Hash of the parent
+  pub parent: Option<BlockHash>,
+  /// Nonce
+  pub nonce: u64,
+  /// State root hash
+  #[serde_as(as = "h256")]
+  pub state_root: TrieHash,
+  /// Transactions root hash
+  #[serde_as(as = "h256")]
+  pub transactions_root: TrieHash,
+  /// Transactions receipts root hash
+  #[serde_as(as = "h256")]
+  pub receipts_root: TrieHash,
+  /// Miner/author's address.
+  #[serde_as(as = "h160")]
+  pub miner: Address,
+  /// Extra data
+  // pub extra_data: Bytes,
+  /// Gas Limit
+  pub gas_limit: u64,
+  /// Gas Used
+  pub gas_used: u64,
+  /// Base fee per unit of gas (if past London)
+  #[serde_as(as = "u256")]
+  pub base_fee_per_gas: GasPrice,
+  /// Base fee per unit of gas (if past London)
+  #[serde_as(as = "u256")]
+  pub next_base_fee_per_gas: GasPrice,
+  /// Timestamp
+  pub timestamp: u64,
+  /// Logs bloom
+  // pub logs_bloom: Bytes,
+  /// Mix Hash
+  pub mix_hash: TrieHash,
+  /// Difficulty
+  pub difficulty: BigInt,
+  /// Difficulty
+  pub total_difficulty: BigInt,
+  pub ommer_count: u64,
+  /// Hash of the uncles
+  #[serde_as(as = "h256")]
+  pub ommer_hash: TrieHash,
+  pub transaction_count: u64,
 }
