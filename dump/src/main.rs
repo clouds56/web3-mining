9369794d03188cc57794442dca3264481932d7e6
Clouds Flowing <clouds.flowing@gmail.com>
2024-03-24 16:00:18 +0800

    init dump

---
+++ dump/src/main.rs
@@ -0,0 +1,18 @@
+use std::str::FromStr as _;
+
+use anyhow::Result;
+use ethers_providers::{JsonRpcClient, Middleware as _, Provider};
+
+async fn get_block_number<P: JsonRpcClient>(client: Provider<P>) -> Result<u64> {
+  let block_number = client.get_block_number().await?;
+  Ok(block_number.as_u64())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  dotenvy::dotenv().ok();
+  let endpoint = format!("http://{}", std::env::var("RETH_HTTP_RPC").as_deref().unwrap_or("127.0.0.1:8545"));
+  let client = Provider::new(ethers_providers::Http::from_str(&endpoint)?);
+  println!("hello, current height is {}", get_block_number(client).await?);
+  Ok(())
+}
