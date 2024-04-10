1b6afe5f90a5e983809b873343f47f8b76445459
Clouds Flowing <clouds.flowing@gmail.com>
2024-04-11 07:28:01 +0800

    support pair

--- tauri-app/src-tauri/src/main.rs
+++ tauri-app/src-tauri/src/main.rs
@@ -3,15 +3,52 @@
 
 #[macro_use] extern crate tracing;
 
-use std::{collections::BTreeMap, fmt::Display, path::PathBuf};
+use std::{collections::{BTreeMap, HashMap}, fmt::Display, path::PathBuf, sync::{Arc, Mutex}};
 
 use polars::{chunked_array::ops::SortOptions, frame::DataFrame, lazy::frame::{IntoLazy, LazyFrame}};
+use polars_plan::dsl::Expr;
 use tauri::State;
 use tracing_subscriber::fmt::format::FmtSpan;
+
+struct ExprLoader {
+  inner: Arc<Mutex<HashMap<String, Vec<Expr>>>>,
+}
+
+impl ExprLoader {
+  fn new() -> Self {
+    Self {
+      inner: Arc::new(Mutex::new(HashMap::new())),
+    }
+  }
+
+  fn insert(&self, name: String, exprs: Vec<Expr>) {
+    self.inner.lock().unwrap().insert(name, exprs);
+  }
+
+  fn get(&self, name: &str) -> Option<Vec<Expr>> {
+    self.inner.lock().unwrap().get(name).cloned()
+  }
+}
+
+impl std::fmt::Debug for ExprLoader {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self.inner.try_lock() {
+      Ok(inner) => {
+        let mut f = f.debug_struct("ExprLoader");
+        for (k, v) in inner.iter() {
+          f.field(k, &v.len());
+        }
+        f.finish()
+      }
+      Err(_) => f.debug_tuple("ExprLoader").field(&"locked").finish(),
+    }
+  }
+}
 
 #[derive(Debug)]
 struct Config {
   data_dir: PathBuf,
+  exprs: ExprLoader,
 }
 
 #[derive(serde::Serialize)]
@@ -59,6 +96,8 @@ async fn list_data_names(config: State<'_, Config>) -> Result<Vec<Dataset>> {
     if let Some((name, idx)) = split_name(&filename, "parquet") {
       map.entry(name.to_string()).or_default().push((idx, filename));
       continue;
+    } else if filename.ends_with(".parquet") {
+      map.entry(filename.to_string()).or_default().push((0, filename));
     }
   }
   Ok(map
@@ -69,6 +108,10 @@ async fn list_data_names(config: State<'_, Config>) -> Result<Vec<Dataset>> {
 
 fn get_data_info(config: &Config, name: &str) -> Result<Dataset> {
   let mut collection = Vec::new();
+  if name.ends_with(".parquet") {
+    collection.push((0, name.to_string()));
+    return Ok(Dataset::new(name.to_string(), collection))
+  }
   for i in std::fs::read_dir(&config.data_dir)? {
     let i = i?;
     let filename = i.file_name().to_string_lossy().to_string();
@@ -84,6 +127,51 @@ fn get_data_info(config: &Config, name: &str) -> Result<Dataset> {
   Ok(Dataset::new(name.to_string(), collection))
 }
 
+fn exprs_from_str(s: &str) -> Result<Vec<polars_plan::dsl::Expr>> {
+  Ok(s.lines().map(|i| serde_json::from_str(i)).collect::<Result<Vec<_>, _>>()?)
+}
+
+fn infer_expr_from_schema(schema: &polars::prelude::Schema) -> Result<Vec<Expr>> {
+  use polars::prelude::DataType;
+  use polars::lazy::dsl::*;
+  let mut exprs = Vec::new();
+  for i in schema.iter_fields() {
+    match i.data_type() {
+      DataType::Int64 => exprs.push(col(i.name())),
+      DataType::Float64 => {
+        exprs.push(col(i.name()).mean().alias(&format!("{}:mean", &i.name)));
+      }
+      _ => {},
+    }
+    exprs.push(col(i.name()).fill_null_with_strategy(polars::chunked_array::ops::FillNullStrategy::Forward(None)).last())
+  }
+  Ok(exprs)
+}
+
+#[instrument(level="debug", skip_all, fields(name=%name))]
+fn load_exprs(config: &Config, name: &str, schema: Option<&polars::prelude::Schema>) -> Result<Vec<Expr>> {
+  let exprs = match config.exprs.get(name) {
+    Some(exprs) => exprs,
+    None => {
+      let exprs = match name {
+        _ if name.starts_with("block_metrics_") => exprs_from_str(include_str!("../exprs/bm.jsonl"))?,
+        _ if name.starts_with("uniswap_pair_block_") => exprs_from_str(include_str!("../exprs/upair.jsonl"))?,
+        _ => if let Some(schema) = schema {
+          infer_expr_from_schema(schema)?
+        } else {
+          return Err("no schema to infer expr".into())
+        }
+      };
+      config.exprs.insert(name.to_string(), exprs.clone());
+      exprs
+    }
+  };
+
+  // TODO: filter invalid exprs here
+  info!(?exprs);
+  Ok(exprs)
+}
+
 #[derive(serde::Serialize)]
 struct Data {
   name: String,
@@ -94,7 +182,7 @@ struct Data {
 #[instrument(level="info", fields(data_dir=%config.data_dir.display(), ok=config.data_dir.is_dir()))]
 async fn get_data(config: State<'_, Config>, name: String) -> Result<Data> {
   use polars::{datatypes::*, lazy::dsl::*};
-  use std::ops::Mul;
+  use std::ops::*;
   let info = get_data_info(&config, &name)?;
   if info.collection.is_empty() {
     return Err("nothing in collection".into())
@@ -104,20 +192,30 @@ async fn get_data(config: State<'_, Config>, name: String) -> Result<Data> {
     LazyFrame::scan_parquet(&path, Default::default())?.collect()
   }).collect::<Result<Vec<_>, _>>()?;
   let mut df = None::<DataFrame>;
+  let mut schema = None;
+  let mut exprs = None;
   for b in df_s {
     df = Some(match df {
       Some(a) => a.vstack(&b)?,
-      None => b
+      None => {
+        schema = Some(b.schema());
+        exprs = Some(load_exprs(&config, &name, Some(&b.schema()))?);
+        b
+      }
     })
   }
   let df = df.unwrap().lazy();
   info!(df=%df.clone().limit(10).collect()?.head(None));
-  let expr_jsonl = include_str!("../exprs/bm.jsonl");
-  let exprs = expr_jsonl
-    .lines().filter(|i| !i.trim().is_empty())
-    .map(|i| serde_json::from_str(i))
-    .collect::<Result<Vec<_>, _>>()?;
-  info!(?exprs);
+
+  let schema = schema.ok_or("no schema")?;
+  let exprs = exprs.ok_or("no exprs")?;
+
+  let df = if schema.get("timestamp").is_none() {
+    // TODO: cache from block_metrics
+    df.with_column(col("height").mul(lit(15)).add(lit(1438269973)).alias("timestamp"))
+  } else {
+    df
+  };
   let df = df
     .filter(col("timestamp").gt(lit(0)))
     .with_column(col("timestamp").mul(lit(1000)).cast(DataType::Datetime(TimeUnit::Milliseconds, None)).cast(DataType::Date).alias("_date"))
@@ -148,6 +246,7 @@ fn main() {
     .init();
   let config = Config {
     data_dir: "data".into(),
+    exprs: ExprLoader::new(),
   };
   // if std::env::current_dir().unwrap().to_string_lossy() == std::env::var("CARGO_MANIFEST_DIR").unwrap() {
   //   std::env::set_current_dir(std::env::var("CARGO_WORKSPACE_DIR").unwrap()).unwrap();
--- test/test_df.py
+++ test/test_df.py
@@ -1,6 +1,9 @@
 # %%
 import os
+from typing import List
+import itertools
 import polars as pl
+import matplotlib.pyplot as plt
 from pathlib import Path
 while not os.path.exists("Cargo.toml"):
   os.chdir("../")
@@ -20,55 +23,104 @@ def load_files(files) -> pl.DataFrame:
   return dfa
 
 # %%
-df = load_files("block_metrics_*.parquet")
+def try_int(s: str):
+  try:
+    return int(s)
+  except:
+    return None
+files = pl.DataFrame({
+  'path': Path("data").rglob("*.parquet")
+}).with_columns([
+  pl.col('path').map_elements(lambda x: x.name.split(".")[0]).alias('prefix'),
+  pl.col('path').map_elements(lambda x: try_int(x.name.split(".")[1])).alias('idx'),
+  pl.col('path').map_elements(lambda x: str(x)).alias('path'),
+]).with_columns([
+  pl.col('prefix').map_elements(lambda x: try_int(x.split("_")[-1])).alias('cut'),
+]).sort('prefix', 'idx')
+datasets = files.group_by('prefix').agg([
+  pl.first('cut'),
+  pl.max('idx').alias('max'),
+  pl.count('idx').alias('count'),
+  pl.col('path').alias('paths'),
+]).with_columns([
+  pl.col('prefix')
+    .str.strip_suffix(pl.col('cut').cast(pl.String))
+    .str.strip_suffix('_')
+    .fill_null(pl.col('prefix'))
+    .alias('name')
+]).sort('name')
+def load_datasets(name: str) -> pl.DataFrame:
+  filenames = datasets.filter(pl.col('name') == name)['paths'].explode()
+  prefix = "".join([list(x)[0] for x in itertools.takewhile(lambda x: len(x) == 1, map(set, zip(*filenames)))])
+  print("load", prefix, list(filenames.str.strip_prefix(prefix)))
+  return load_files(filenames)
+datasets
+
+# %%
+# df = load_files("block_metrics_*.parquet")
+df = load_datasets('block_metrics')
 df.mean()
 
 # %%
-df = load_files("uniswap_factory_*.parquet")
+df = load_datasets("uniswap_factory_events")
 df.sort('height')['tx_hash'].head().to_list()
 
 # %%
-files = Path("data").rglob("uniswap_pair_old_*.parquet")
-file = list(files)[0]
-df = pl.read_parquet(file)
-dfg = df.group_by('topic0').agg([pl.first('tx_hash'), pl.count('height').alias('count')]).sort('count', descending=True)
-list(zip(*[list(dfg[col]) for col in dfg.columns]))
+df = load_files("uniswap_pair_old_*.parquet")
+if df:
+  dfg = df.group_by('topic0').agg([pl.first('tx_hash'), pl.count('height').alias('count')]).sort('count', descending=True)
+  list(zip(*[list(dfg[col]) for col in dfg.columns]))
 
 # %%
-files = Path("data").rglob("uniswap_pair_0x*.parquet")
-file = list(files)[0]
-df = pl.read_parquet(file)
-df.group_by("action").count().sort("count", descending=True)
+pairs = (datasets
+  .filter(pl.col('name').str.starts_with('uniswap_pair_events'))
+  .select(pl.col('name').str.strip_prefix('uniswap_pair_events_').alias('pair'))
+  ['pair'])
+pairs
 
 # %%
-df_clean = df.group_by('height').agg(
-  (pl.col('value0_in').fill_null(0) - pl.col('value0_out').fill_null(0)).sum(),
-  (pl.col('amount0_in').fill_null(0) - pl.col('amount0_out').fill_null(0)).sum(),
-  (pl.col('amount1_in').fill_null(0) - pl.col('amount1_out').fill_null(0)).sum(),
-  pl.col('reserve0').fill_null(strategy='forward').last(),
-  pl.col('reserve1').fill_null(strategy='forward').last(),
-).sort('height').with_columns(
-  pl.col('value0_in').cum_sum().alias('value0'),
-)
-df_clean.write_parquet("uniswap_pair_0x0.parquet")
+for pair in pairs:
+  df = load_datasets("uniswap_pair_events_" + pair)
+  df.group_by("action").count().sort("count", descending=True)
+
+  df_acc = df.group_by('height').agg(
+    (pl.col('value_in').fill_null(0) - pl.col('value_out').fill_null(0)).sum(),
+    (pl.col('amount0_in').fill_null(0) - pl.col('amount0_out').fill_null(0)).sum(),
+    (pl.col('amount1_in').fill_null(0) - pl.col('amount1_out').fill_null(0)).sum(),
+    pl.col('reserve0').fill_null(strategy='forward').last(),
+    pl.col('reserve1').fill_null(strategy='forward').last(),
+  ).sort('height').with_columns(
+    pl.col('value_in').cum_sum().alias('value'),
+  )
+  df_acc.write_parquet(f"uniswap_pair_block_{pair}.parquet")
+
+  # plt.plot(df_acc['height'], df_acc['value0'])
+  plt.plot(df_acc['height'], (df_acc['reserve0']*df_acc['reserve1']).sqrt()/df_acc['value'], label=pair)
+plt.legend()
+plt.show()
 
 # %%
-exprs = [
+def save_jsonl(filename, exprs: List[pl.Expr]):
+  expr_lines = [x.meta.serialize() for x in exprs]
+  with open(f"{FOLDER_EXPRS}/{filename}", "w") as f:
+    for i in expr_lines:
+      print(i, file=f)
+save_jsonl("bm.jsonl", [
   pl.col("total_eth").sum().alias("total_eth"),
   pl.col("tx_count").cast(pl.UInt64).sum().alias("tx_count"),
   pl.col("total_fee").mean().alias("total_fee"),
   pl.col("gas_used").mean().alias("gas_used"),
   pl.col("fee_per_gas").mean().alias("fee_per_gas:mean"),
   pl.col("fee_per_gas").median().alias("fee_per_gas:median"),
-]
-expr_lines = [x.meta.serialize() for x in exprs]
-with open(f"{FOLDER_EXPRS}/bm.jsonl", "w") as f:
-  for i in expr_lines:
-    print(i, file=f)
-
-# %%
-import matplotlib.pyplot as plt
-# plt.plot(df_clean['height'], df_clean['value0'])
-plt.plot(df_clean['height'], (df_clean['reserve0']*df_clean['reserve1']).sqrt()/df_clean['value0'])
+])
+save_jsonl("upair.jsonl", [
+  pl.sum("value_in"),
+  pl.sum("amount0_in"),
+  pl.sum("amount1_in"),
+  pl.last("reserve0"),
+  pl.last("reserve1"),
+  pl.last("value"),
+  ((pl.col('reserve0') * pl.col("reserve1")).sqrt() / pl.col("value")).last().alias("scale"),
+])
 
 # %%
